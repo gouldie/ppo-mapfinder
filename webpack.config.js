9c90cce60843b348844df000368080db1185871a
Matt <matt@matts-mbp.home>
2017-06-16 18:46:26 +0100

    update to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-    entry: [
-        './src/index.js'
-    ],
+    entry: {
+        app: './src/index.js'
+    },
     output: {
         path: __dirname,
         publicPath: '/',
         filename: 'bundle.js'
     },
     module: {
-        loaders: [{
-            test: /.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'react']
-            }
-        }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
+    	rules: [
+    		{
+			test: /.jsx?$/,
+			exclude: /node_modules/,
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015', 'react']
+
+				}
+			}]
+		}],
+
     }
 };
