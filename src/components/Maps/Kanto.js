f68e24ae25768b5d1a4877d890babb861a0712ee
Matt <matt@matts-mbp.home>
2017-06-18 17:12:38 +0100

    data

--- src/components/Maps/Kanto.js
+++ src/components/Maps/Kanto.js
@@ -99,6 +99,114 @@ export default class Kanto extends Component {
 					setSelectedArea(['viridian-forest-1', 'viridian-forest-2'])
 				}}></div>
 
+				<div className='cerulean-city' data-tip="Cerulean City" style={{
+					cursor: 'pointer',
+					position: 'absolute',
+					top: '45px',
+					left: '298px',
+					width: '28px',
+					height: '29px',
+					borderRadius: '50%',
+					backgroundColor: mode === 0 ? selectedArea.indexOf('cerulean-city') >= 0 ? 'indianred' : ''
+						: matchedAreas && matchedAreas.indexOf('cerulean-city') >= 0 ? 'indianred' : '' ,
+					animation: mode === 0 ? selectedArea.indexOf('cerulean-city') >= 0 ? 'pulse 2s infinite' : ''
+						: matchedAreas && matchedAreas.indexOf('cerulean-city') >= 0 ? 'pulse 2s infinite' : ''
+				}} onClick={(e) => {
+					if (mode === 1) return
+					if (e.stopPropagation) e.stopPropagation()
+					setSelectedArea(['cerulean-city'])
+				}}></div>
+
+				<div className='vermillion-city' data-tip="Vermillion City" style={{
+					cursor: 'pointer',
+					position: 'absolute',
+					top: '177px',
+					left: '298px',
+					width: '28px',
+					height: '29px',
+					borderRadius: '50%',
+					backgroundColor: mode === 0 ? selectedArea.indexOf('vermillion-city') >= 0 ? 'indianred' : ''
+						: matchedAreas && matchedAreas.indexOf('vermillion-city') >= 0 ? 'indianred' : '' ,
+					animation: mode === 0 ? selectedArea.indexOf('vermillion-city') >= 0 ? 'pulse 2s infinite' : ''
+						: matchedAreas && matchedAreas.indexOf('vermillion-city') >= 0 ? 'pulse 2s infinite' : ''
+				}} onClick={(e) => {
+					if (mode === 1) return
+					if (e.stopPropagation) e.stopPropagation()
+					setSelectedArea(['vermillion-city'])
+				}}></div>
+
+				<div className='celadon-city' data-tip="Celadon City" style={{
+					cursor: 'pointer',
+					position: 'absolute',
+					top: '110px',
+					left: '232px',
+					width: '28px',
+					height: '29px',
+					borderRadius: '50%',
+					backgroundColor: mode === 0 ? selectedArea.indexOf('celadon-city') >= 0 ? 'indianred' : ''
+						: matchedAreas && matchedAreas.indexOf('celadon-city') >= 0 ? 'indianred' : '' ,
+					animation: mode === 0 ? selectedArea.indexOf('celadon-city') >= 0 ? 'pulse 2s infinite' : ''
+						: matchedAreas && matchedAreas.indexOf('celadon-city') >= 0 ? 'pulse 2s infinite' : ''
+				}} onClick={(e) => {
+					if (mode === 1) return
+					if (e.stopPropagation) e.stopPropagation()
+					setSelectedArea(['celadon-city'])
+				}}></div>
+
+				<div className='fuchsia-city' data-tip="Fuchsia City" style={{
+					cursor: 'pointer',
+					position: 'absolute',
+					top: '243px',
+					left: '255px',
+					width: '28px',
+					height: '29px',
+					borderRadius: '50%',
+					backgroundColor: mode === 0 ? selectedArea.indexOf('fuchsia-city') >= 0 ? 'indianred' : ''
+						: matchedAreas && matchedAreas.indexOf('fuchsia-city') >= 0 ? 'indianred' : '' ,
+					animation: mode === 0 ? selectedArea.indexOf('fuchsia-city') >= 0 ? 'pulse 2s infinite' : ''
+						: matchedAreas && matchedAreas.indexOf('fuchsia-city') >= 0 ? 'pulse 2s infinite' : ''
+				}} onClick={(e) => {
+					if (mode === 1) return
+					if (e.stopPropagation) e.stopPropagation()
+					setSelectedArea(['fuchsia-city'])
+				}}></div>
+
+				<div className='cinnabar-island' data-tip="Cinnabar Island" style={{
+					cursor: 'pointer',
+					position: 'absolute',
+					top: '285px',
+					left: '80px',
+					width: '28px',
+					height: '28px',
+					borderRadius: '50%',
+					backgroundColor: mode === 0 ? selectedArea.indexOf('cinnabar-island') >= 0 ? 'indianred' : ''
+						: matchedAreas && matchedAreas.indexOf('cinnabar-island') >= 0 ? 'indianred' : '' ,
+					animation: mode === 0 ? selectedArea.indexOf('cinnabar-island') >= 0 ? 'pulse 2s infinite' : ''
+						: matchedAreas && matchedAreas.indexOf('cinnabar-island') >= 0 ? 'pulse 2s infinite' : ''
+				}} onClick={(e) => {
+					if (mode === 1) return
+					if (e.stopPropagation) e.stopPropagation()
+					setSelectedArea(['cinnabar-island'])
+				}}></div>
+
+				<div className='mt-moon' data-tip="Mt. Moon" style={{
+					cursor: 'pointer',
+					position: 'absolute',
+					top: '50px',
+					left: '193px',
+					width: '18px',
+					height: '18px',
+					borderRadius: '50%',
+					backgroundColor: mode === 0 ? selectedArea.indexOf('mt-moon') >= 0 ? 'indianred' : ''
+						: matchedAreas && matchedAreas.indexOf('mt-moon') >= 0 ? 'indianred' : '' ,
+					animation: mode === 0 ? selectedArea.indexOf('mt-moon') >= 0 ? 'pulse 2s infinite' : ''
+						: matchedAreas && matchedAreas.indexOf('mt-moon') >= 0 ? 'pulse 2s infinite' : ''
+				}} onClick={(e) => {
+					if (mode === 1) return
+					if (e.stopPropagation) e.stopPropagation()
+					setSelectedArea(['mt-moon', 'mt-moon-exit'])
+				}}></div>
+
       </div>
     )
   }
