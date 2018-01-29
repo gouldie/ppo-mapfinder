96c5c19cc60302ef15a71a514df14b4891b6a52f
Matt <matt@Matts-MacBook-Pro.local>
2018-01-29 12:54:48 +0000

    sinnoh wip

--- src/components/Maps/Sinnoh.js
+++ src/components/Maps/Sinnoh.js
@@ -247,6 +247,259 @@ export default class Johto extends Component {
           setSelectedArea(['oreburgh-gate', 'deep-oreburgh-mine'])
         }}></div>
 
+        <div className="route-205" data-tip="Route 205" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '198px',
+          left: '108px',
+          width: '23px',
+          height: '8px',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('route-205') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('route-205') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('route-205') >= 0 ? 'pulse 2s infinite' : ''
+            : matchedAreas && matchedAreas.indexOf('route-205') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['route-205'])
+        }}>
+        </div>
+        <div className="route-205" data-tip="Route 205" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '154px',
+          left: '123px',
+          width: '8px',
+          height: '45px',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('route-205') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('route-205') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('route-205') >= 0 ? 'pulse 2s infinite' : ''
+            : matchedAreas && matchedAreas.indexOf('route-205') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['route-205'])
+        }}>
+        </div>
+        <div className="route-205" data-tip="Route 205" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '147px',
+          left: '115px',
+          width: '23px',
+          height: '8px',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('route-205') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('route-205') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('route-205') >= 0 ? 'pulse 2s infinite' : ''
+            : matchedAreas && matchedAreas.indexOf('route-205') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['route-205'])
+        }}>
+        </div>
+
+        <div className='Fuego Ironworks' data-tip="Fuego Ironworks" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '138px',
+          left: '87px',
+          width: '27px',
+          height: '27px',
+          borderRadius: '50%',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('fuego-ironworks') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('fuego-ironworks') >= 0
+            || matchedAreas && matchedAreas.indexOf('fuego-ironworks-factory') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('fuego-ironworks') >= 0 ? 'pulse 2s infinite' : ''
+            : matchedAreas && matchedAreas.indexOf('fuego-ironworks') >= 0
+            || matchedAreas && matchedAreas.indexOf('fuego-ironworks-factory') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['fuego-ironworks', 'fuego-ironworks-factory'])
+        }}></div>
+
+        <div className='Iron Island' data-tip="Iron Island" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '123px',
+          left: '53px',
+          width: '17px',
+          height: '17px',
+          borderRadius: '50%',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('iron-island') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('iron-island') >= 0
+            || matchedAreas && matchedAreas.indexOf('iron-cave') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('iron-island') >= 0 ? 'pulse 2s infinite' : ''
+            : matchedAreas && matchedAreas.indexOf('iron-island') >= 0
+            || matchedAreas && matchedAreas.indexOf('iron-cave') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['iron-island', 'iron-cave'])
+        }}></div>
+
+        <div className='Eterna Forest' data-tip="Eterna Forest" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '131px',
+          left: '118px',
+          width: '17px',
+          height: '17px',
+          borderRadius: '50%',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('eterna-forest') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('eterna-forest') >= 0
+            || matchedAreas && matchedAreas.indexOf('old-chateau') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('eterna-forest') >= 0 ? 'pulse 2s infinite' : ''
+            : matchedAreas && matchedAreas.indexOf('eterna-forest') >= 0
+            || matchedAreas && matchedAreas.indexOf('old-chateau') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['eterna-forest', 'old-chateau'])
+        }}></div>
+
+        <div className="route-211" data-tip="Route 211" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '147px',
+          left: '168px',
+          width: '23px',
+          height: '8px',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('route-211') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('route-211') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('route-211') >= 0 ? 'pulse 2s infinite' : ''
+            : matchedAreas && matchedAreas.indexOf('route-211') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['route-211'])
+        }}>
+        </div>
+        <div className="route-211" data-tip="Route 211" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '147px',
+          left: '196px',
+          width: '17px',
+          height: '8px',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('route-211') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('route-211') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('route-211') >= 0 ? 'pulse 2s infinite' : ''
+            : matchedAreas && matchedAreas.indexOf('route-211') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['route-211'])
+        }}>
+        </div>
+
+        <div className="Mt. Coronet" data-tip="Mt. Coronet" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '95px',
+          left: '190px',
+          width: '8px',
+          height: '97px',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('mt-coronet-center') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('mt-coronet-center') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-tunnel') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-south') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-north') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-summit') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-2f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-3f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-4f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-b1f') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('mt-coronet-center') >= 0 ? 'pulse 2s infinite' : ''
+          : matchedAreas && matchedAreas.indexOf('mt-coronet-center') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-tunnel') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-south') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-north') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-summit') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-2f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-3f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-4f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-b1f') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['mt-coronet-center', 'mt-coronet-tunnel', 'mt-coronet-south',
+          'mt-coronet-north', 'mt-coronet-summit', 'mt-coronet-2f', 'mt-coronet-3f',
+          'mt-coronet-4f', 'mt-coronet-b1f'])
+        }}>
+        </div>
+        <div className="Mt. Coronet" data-tip="Mt. Coronet" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '186px',
+          left: '175px',
+          width: '8px',
+          height: '39px',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('mt-coronet-center') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('mt-coronet-center') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-tunnel') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-south') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-north') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-summit') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-2f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-3f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-4f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-b1f') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('mt-coronet-center') >= 0 ? 'pulse 2s infinite' : ''
+          : matchedAreas && matchedAreas.indexOf('mt-coronet-center') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-tunnel') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-south') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-north') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-summit') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-2f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-3f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-4f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-b1f') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['mt-coronet-center', 'mt-coronet-tunnel', 'mt-coronet-south',
+          'mt-coronet-north', 'mt-coronet-summit', 'mt-coronet-2f', 'mt-coronet-3f',
+          'mt-coronet-4f', 'mt-coronet-b1f'])
+        }}>
+        </div>
+        <div className="Mt. Coronet" data-tip="Mt. Coronet" style={{
+          cursor: mode === 0 ? 'pointer' : 'default',
+          position: 'absolute',
+          top: '185px',
+          left: '182px',
+          width: '8px',
+          height: '8px',
+          backgroundColor: mode === 0 ? selectedArea.indexOf('mt-coronet-center') >= 0 ? 'indianred' : ''
+            : matchedAreas && matchedAreas.indexOf('mt-coronet-center') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-tunnel') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-south') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-north') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-summit') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-2f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-3f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-4f') >= 0
+            || matchedAreas && matchedAreas.indexOf('mt-coronet-b1f') >= 0 ? 'indianred' : '' ,
+          animation: mode === 0 ? selectedArea.indexOf('mt-coronet-center') >= 0 ? 'pulse 2s infinite' : ''
+          : matchedAreas && matchedAreas.indexOf('mt-coronet-center') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-tunnel') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-south') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-north') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-summit') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-2f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-3f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-4f') >= 0
+          || matchedAreas && matchedAreas.indexOf('mt-coronet-b1f') >= 0 ? 'pulse 2s infinite' : ''
+        }} onClick={(e) => {
+          if (mode === 1) return
+          if (e.stopPropagation) e.stopPropagation()
+          setSelectedArea(['mt-coronet-center', 'mt-coronet-tunnel', 'mt-coronet-south',
+          'mt-coronet-north', 'mt-coronet-summit', 'mt-coronet-2f', 'mt-coronet-3f',
+          'mt-coronet-4f', 'mt-coronet-b1f'])
+        }}>
+        </div>
+
       </div>
     )
   }
