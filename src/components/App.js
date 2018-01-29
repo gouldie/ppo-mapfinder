e0e45c7863849b4e5f707fa9bf9575f99d7f1d25
Matt <matt@Matts-MacBook-Pro.local>
2018-01-29 14:23:49 +0000

    kanto default map

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
 
     this.state = {
       mode: 0, // 0: by map, 1: by pokemon
-      selectedMap: 'Sinnoh',
+      selectedMap: 'Kanto',
       selectedArea: [],
       selectedPokemon: null,
     }
